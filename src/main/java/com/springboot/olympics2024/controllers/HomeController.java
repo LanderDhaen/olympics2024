7dacca96eaea0070970cc9a282fa452cf717fd20
LanderDhaen <lander.dhaen@student.hogent.be>
2024-05-15 21:37:01 +0200

    feat(homepage): Improved the homepage with sports pulled from the database

--- src/main/java/com/springboot/olympics2024/controllers/HomeController.java
+++ src/main/java/com/springboot/olympics2024/controllers/HomeController.java
@@ -1,16 +1,26 @@
 package com.springboot.olympics2024.controllers;
 
 
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import repository.SportRepository;
 
 @Controller
-
+@RequestMapping("olympics2024")
 public class HomeController {
 
-    @GetMapping("/olympics2024")
-    public String olympics2024() {
-        return "olympics2024";
+   @Autowired
+   private SportRepository sportRepository;
+
+   @GetMapping
+    public String olympics2024(Model model) {
+
+       model.addAttribute("sports", sportRepository.findAll());
+
+       return "olympics2024";
     }
 
 }
