dbd06393b81fa516851f2540b91894a65c71244e
LanderDhaen <lander.dhaen@student.hogent.be>
2024-05-20 21:24:52 +0200

    feat(tickets): Made the tickets button on the homepage only visible if the logged in user has tickets

--- src/main/java/com/springboot/olympics2024/controllers/HomeController.java
+++ src/main/java/com/springboot/olympics2024/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package com.springboot.olympics2024.controllers;
 
 
+import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import service.SpectatorService;
 import service.SportService;
 
 @Controller
@@ -15,10 +17,14 @@ public class HomeController {
    @Autowired
    private SportService sportService;
 
+   @Autowired
+   private SpectatorService spectatorService;
+
    @GetMapping
-    public String olympics2024(Model model) {
+    public String olympics2024(Model model, Authentication authentication) {
 
        model.addAttribute("sports", sportService.findAllSports());
+       model.addAttribute("tickets", spectatorService.findByUsername(authentication.getName()).getTickets());
 
        return "olympics2024";
     }
