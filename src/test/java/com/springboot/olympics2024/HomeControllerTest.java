2dec7c9c24c6a9bc896f2004d0c89145e426ae3d
LanderDhaen <lander.dhaen@student.hogent.be>
2024-05-23 23:54:54 +0200

    test(testing): Improved the tests for HomeController

--- src/test/java/com/springboot/olympics2024/HomeControllerTest.java
+++ src/test/java/com/springboot/olympics2024/HomeControllerTest.java
@@ -22,11 +22,28 @@ public class HomeControllerTest {
 
     @Test
     @WithMockUser(username = "User", roles = { "USER" })
-    public void testGetRequest() throws Exception {
+    public void testGetRequestAsUser() throws Exception {
         mockMvc.perform(get("/olympics2024"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("olympics2024"))
                 .andExpect(model().attributeExists("sports"))
                 .andExpect(model().attributeExists("tickets"));
+    }
+
+    @Test
+    @WithMockUser(username = "Admin", roles = { "ADMIN" })
+    public void testGetRequestAsAdmin() throws Exception {
+        mockMvc.perform(get("/olympics2024"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("olympics2024"))
+                .andExpect(model().attributeExists("sports"))
+                .andExpect(model().attributeExists("tickets"));
+    }
+
+    @Test
+    public void testGetRequestWithoutUser() throws Exception {
+        mockMvc.perform(get("/olympics2024"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
     }
 }
