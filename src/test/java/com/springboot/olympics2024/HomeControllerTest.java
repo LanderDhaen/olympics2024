68c9280f6823f656e5f33cdf83fe72a14b5b1c53
LanderDhaen <lander.dhaen@student.hogent.be>
2024-05-23 23:04:26 +0200

    test(testing): Added test for HomeController

---
+++ src/test/java/com/springboot/olympics2024/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.springboot.olympics2024;
+
+import com.springboot.olympics2024.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(SecurityConfig.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "User", roles = { "USER" })
+    public void testGetRequest() throws Exception {
+        mockMvc.perform(get("/olympics2024"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("olympics2024"))
+                .andExpect(model().attributeExists("sports"))
+                .andExpect(model().attributeExists("tickets"));
+    }
+}
